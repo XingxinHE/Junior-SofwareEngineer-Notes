9127d9f9399f1777c38a1644ba6dcac395796b08
Xingxin HE <xingxin.he@mail.polimi.it>
2022-10-22 15:32:21 +0800

    computation time

--- code/GeometryProcessing/csc2520_jacobson/geometry-processing-introduction/main.cpp
+++ code/GeometryProcessing/csc2520_jacobson/geometry-processing-introduction/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "edges.h"
 #include "euler_characteristic.h"
 #include "igl/edges.h"
@@ -12,19 +13,29 @@ int main(int argc, char *argv[])
   // Load in a mesh
   igl::read_triangle_mesh(argc>1 ? argv[1] : "../data/bunny.off", V, F);
 
+
   // My Implementation
   std::cout <<"My implementation: " << std::endl;
+  auto start = std::chrono::high_resolution_clock::now();
   Eigen::MatrixXi E = edges(F);
   int Chi = euler_characteristic(F);
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
   std::cout << "Edge list E is "<<E.rows() << "x" << E.cols() << std::endl;
   std::cout << "Euler Characteristic: " << Chi << std::endl;
+  std::cout << "Time: " << duration.count() << std::endl;
 
   // Libigl Implementation
+  std::cout << "Libigl implementation: " << std::endl;
+  start = std::chrono::high_resolution_clock::now();
   Eigen::MatrixXi EIGL;
   igl::edges(F, EIGL);
-  std::cout << "Libigl implementation: " << std::endl;
+  Chi = igl::euler_characteristic(F);
+  stop = std::chrono::high_resolution_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
   std::cout << "Edge list E is " << EIGL.rows() << "x" << EIGL.cols() << std::endl;
-  std::cout << "Euler Characteristic: " << igl::euler_characteristic(F) << std::endl;
+  std::cout << "Euler Characteristic: " << Chi << std::endl;
+  std::cout << "Time: " << duration.count() << std::endl;
 
   // Create a libigl Viewer object 
   igl::opengl::glfw::Viewer viewer;
